30092ba1c70f19b0aaf28601b9546b28a4083e92
Jeremy Alcanzare <jeremyalcanzare32@gmail.com>
2020-06-20 19:27:52 -0500

    added react files in react app folder

---
+++ React App/src/DayScheduler.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DayScheduler() {
+  return <div>text</div>;
+}
---
+++ React App/src/DayTile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DayTile(props) {
+  return <td className="dayTile">{props.text}</td>;
+}
