f46921a96961f91985b5cb29e48ab6dc534f123e
chanwkkk <chanwk18@hotmail.com>
2020-06-20 22:49:29 -0600

    add some functions that doctorRoutes need

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {loginRequired, rightUser, validateUser} = require('../Controllers/authController')
 const {getDoctor, getDoctors, updateDoctor} = require('../controllers/doctorsController')
 
+router.use(loginRequired)
 router.get('/', getDoctors);
 
 router.route('/:doctorId')
     .get(getDoctor)
-    .put(rightDoctor,updateDoctor);// You have to be the right Doctor to change the Doctor
+    .put(rightUser,validateUser, updateDoctor);// You have to be the right Doctor to change the Doctor
 
 router.use('/:doctorId/appointments', appointmentRoutes);
