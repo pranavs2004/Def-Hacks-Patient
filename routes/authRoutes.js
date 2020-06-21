0b26554afd7a15960a5b7484fdaf4b6b6922ac51
chanwkkk <chanwk18@hotmail.com>
2020-06-20 22:02:05 -0600

    add authRoutes

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// import three functions in controller
+const {login, logout, register} = require('../controllers/usersController');
+
+router.post('/login', login);
+router.post('/register', register);
+// Not sure if we need log out or just tell the frontend to clear the JWT token
+router.post('/logout', logout)
+
+
+module.exports = router;
