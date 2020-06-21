85e1547ed037a00a3f7317bca0cd50fbde59ad38
chanwkkk <chanwk18@hotmail.com>
2020-06-21 10:45:49 -0600

    add login action

---
+++ controllers/appointmentsController.js
@@ -0,0 +1,10 @@
+// getAppointment, getAppointments, createAppointment, updateAppointment
+const appointmentFuntion = require('../linked_list');
+const Appoinment = 
+const Mongoose
+
+const getAppointment =......if
+
+
+
+module.exports = {getAppointment, getAppointments, createAppointment, updateAppointment}
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,14 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt  =  require('bcrypt')
 
 const login = async (req, res) => {
-    if(req.user.)
-    User.findOne({email: req.body.email}, (err, user) => {
+    const identity = req.user.identity
+    const capitalized = identity.charAt(0).toUpperCase() + identity.slice(1)
+    capitalized.findOne({email: req.body.email}, (err, user) => {
       if (err) res.json(err.message);
       if (!user) res.json({'message': 'User not found'});
-      else if (user && user.comparePasswords(req.body.password, user.hashPassword)) {
+      else if (user && user.comparePasswords(req.body.password, user.hashPassword)) {//make sure user has hashpassword and comparepasswords in user.js
         res.json({'token': jwt.sign({
           id: user.id,
           email: user.email,
