2136401fdd575b0f045e4d14f99405ac8ba77d26
pranavs2004 <60489437+pranavs2004@users.noreply.github.com>
2020-06-20 23:41:44 -0500

    full changes to model

--- models/doctor.js
+++ models/doctor.js
@@ -6,14 +6,36 @@ var Schema = mongoose.Schema;
 var Doctor = new Schema({
   _id: {
   type:Number,
-   min: [6, 'Too few characters']
-   required:true
- }
- name:String,
+   min: [6, 'Too few characters'],
+   required:true,
+ },
+  email:{
+    type:String,
+    required:true,
+  },
+  username:{
+  type:String,
+  required:true,
+},
+  password: {
+  type:String,
+  required:true,
+  min:[7, "password must be more secure"],
+},
+ firstname:{
+   type:String,
+   required:true,
+ },
+ lastname:{
+   type:String,
+   required:true,
+ },
  speciality: {
  type:String,
- required:true
-}
+ required:true,
+},
+patientsIDs:[Number],
+appointment: [String],
 });
 
 module.exports = mongoose.model('Doctor', Doctor );
--- models/patient.js
+++ models/patient.js
@@ -6,14 +6,38 @@ var Schema = mongoose.Schema;
 var Patient = new Schema({
   _id: {
   type:Number,
-  min: [6, 'Too few characters']
-  required:true
-}
-  name: String,
+  min: [6, 'Too few characters'],
+  required:true,
+},
+  firstname:{
+    type:String,
+    required:true,
+  },
+  lastname:{
+    type:String,
+    required:true,
+  },
+  email_address:{
+    type:String,
+    required:true,
+  },
+  phone_number:{
+    type:Number,
+    required: true,
+  },
+  username:{
+    type:String,
+    required:true,
+  },
+  password:{
+    type:String,
+    required:true,
+  },
   symptoms: {
   type:String,
-}
-
+},
+  appointment:[String],
+  doctorIDs:[Number],
 });
 
 
