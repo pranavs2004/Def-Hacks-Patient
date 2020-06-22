86f491f0d03fe5d07c28aa865c037b0fcf466406
chanwkkk <chanwk18@hotmail.com>
2020-06-21 20:26:32 -0600

    add getAppointments

--- controllers/appointmentsController.js
+++ controllers/appointmentsController.js
@@ -4,12 +4,20 @@ const Appoinment = require('../models/appointments');
 const Mongoose = require('mongoose')
 
 const getAppointment=async (req, res) =>{
-//   Appoinment.find({})
+  Appoinment.find({doctorId: req.params.doctorId})
 };
 
 const getAppointments=async (req, res) =>{
-
+    if(req.params.doctorId) user = "doctorId";
+    else if(req.params.patientId) user = "patientId";
+    try {
+        appointments= Appoinment.find({user: req.params[user]});
+        res.json(appointments);
+        } catch (err) {
+        res.json(err.message);
+        }
 };
+
 const createAppointment=async (req, res) =>{
 
 };
