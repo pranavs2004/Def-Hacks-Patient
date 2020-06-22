84b39cdcb89b41f373bf5de419991800a8ef6156
pranavs2004 <60489437+pranavs2004@users.noreply.github.com>
2020-06-21 21:07:14 -0500

    updated db host

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {loginRequired} = require('./controllers/authController')
 //Set up default mongoose connection
 //mongodb://dbuser:dbpassword@mongo-cluster-shard-00-00-ixqtu.mongodb.net:27017,mongo-cluster-shard-00-01-ixqtu.mongodb.net:27017,mongo-cluster-shard-00-02-ixqtu.mongodb.net:27017/test?ssl=true&replicaSet=mongo-cluster-shard-0&authSource=admin&retryWrites=true
 //var mongoDB = 'mongodb+srv://pranav:pranav123@cluster0-dn2vn.mongodb.net/<local_library>?retryWrites=true&w=majority';
-var mongoDB =mongodb://pranav:pranav123@mongo-cluster-shard-00-00-ixqtu.mongodb.net:27017,mongo-cluster-shard-00-01-ixqtu.mongodb.net:27017,mongo-cluster-shard-00-02-ixqtu.mongodb.net:27017/test?ssl=true&replicaSet=mongo-cluster-shard-0&authSource=admin&retryWrites=true
+var mongoDB ='mongodb://pranav:pranav123@mongo-cluster-shard-00-00-ixqtu.mongodb.net:27017,mongo-cluster-shard-00-01-ixqtu.mongodb.net:27017,mongo-cluster-shard-00-02-ixqtu.mongodb.net:27017/test?ssl=true&replicaSet=mongo-cluster-shard-0&authSource=admin&retryWrites=true'
 
 
 // Connect the mongoose with database
