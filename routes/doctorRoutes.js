2d520489f7bcdc0f1388b57ee3ff255d31c7b986
chanwkkk <chanwk18@hotmail.com>
2020-06-21 20:05:44 -0600

    add getPatients to doctor routes

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {rightUser} = require('../controllers/authController')
+const {rightDoctor} = require('../controllers/authController')
 const {getDoctor, getDoctors, updateDoctor} = require('../controllers/doctorsController')
-const appointmentRoutes = require('./appointmentRoutes')
+const appointmentRoutes = require('./appointmentRoutes');
+const { getPatients } = require('../controllers/patientsController');
 
 router.get('/', getDoctors);
 
 router.route('/:doctorId')
     .get(getDoctor)
-    .put(rightUser, updateDoctor);// You have to be the right Doctor to change the Doctor
+    .put(rightDoctor, updateDoctor);// You have to be the right Doctor to change the Doctor
 
-router.use('/:doctorId/appointments', rightUser, appointmentRoutes);
+
+router.use('/:doctorId/appointments', rightDoctor, appointmentRoutes);
+
+router.get('/:doctorId/patients', rightDoctor, getPatients)
+
 
 
 module.exports = router;
